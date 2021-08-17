e860d99737845b630469f295b748a2821e4f8f32
Anne Wong <directtoanne@gmail.com>
2021-08-17 20:32:41 +0100

    very basic, ugly ass but working account actions and respective views

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,31 @@
 class AccountsController < ApplicationController
+  def index
+    @accounts = Account.where(user_id: current_user.id)
+  end
+
+  def show
+    @account = Account.find(params[:id])
+  end
+
+  def new
+    @account = Account.new
+  end
+  
+  def create
+    @account = Account.new(account_strong_params)
+    @account.user_id = current_user.id
+    @account.save ? (redirect_to account_path(@account)) : (render :new)
+  end
+
+  def destroy
+    @account = Account.find(params[:id])
+    @account.destroy
+    redirect_to accounts_path
+  end
+  
+  def account_strong_params
+    params.require(:account).permit(
+      :user_id, :currency, :bank_name, :account_number, :sort_code
+    )
+  end
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,7 @@
 class Account < ApplicationRecord
+  belongs_to :user
+  has_many :transactions
+  has_many :goals_transactions
+
+  validates :user_id, :currency, :bank_name, :account_number, :sort_code, presence: true
 end
