8c9f73607741e5eff739cda97fbe8e1b3def2580
Jake Monger <jakemonger18@gmail.com>
2021-08-19 18:43:19 +0100

    Added Completed to Goal

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -4,4 +4,16 @@ class Goal < ApplicationRecord
   def goal_percent_complete
     100 * current_amount.to_f / target_amount.to_f
   end
+
+  def current_status
+    if completed?
+      "Complete"
+    elsif target_due_date > Time.now
+      "Overdue"
+    elsif current_amount.zero?
+      "Not Started"
+    else
+      "Active"
+    end
+  end
 end
