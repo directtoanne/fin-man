c89912cd141a20ac29b08a6a01bc6d88f1ca7328
Jake Monger <jakemonger18@gmail.com>
2021-08-21 14:13:45 +0100

    Moved goal_status to private

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -2,19 +2,12 @@ class Goal < ApplicationRecord
   belongs_to :user
 
   def goal_percent_complete
-    100 * current_amount.to_f / target_amount.to_f
+    percent = 100 * current_amount.to_f / target_amount.to_f
+    return percent.round(2)
   end
 
   def current_status
-    if completed?
-      "Complete"
-    elsif target_due_date.past?
-      "Overdue"
-    elsif current_amount == 0.to_f
-      "Not Started"
-    else
-      "Active"
-    end
+    goal_status
   end
 
   def pretty_date(time_date)
@@ -26,6 +19,34 @@ class Goal < ApplicationRecord
       "bg-danger"
     else
       "bg-success"
+    end
+  end
+
+  # Set categorys for te Goals
+  def goal_categories
+    [
+      "Car",
+      "Debt",
+      "Education",
+      "Holiday",
+      "Home",
+      "Medical",
+      "Savings",
+      "Other"
+    ]
+  end
+
+  private
+
+  def goal_status
+    if completed?
+      "Complete"
+    elsif target_due_date.past?
+      "Overdue"
+    elsif current_amount == 0.to_f
+      "Not Started"
+    else
+      "Active"
     end
   end
 end
