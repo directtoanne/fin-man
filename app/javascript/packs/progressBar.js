218e8db6850b437c6ac23b9c7a079cae780e7bd2
Jake Monger <jakemonger18@gmail.com>
2021-08-26 00:39:30 +0100

    js formatting

--- app/javascript/packs/progressBar.js
+++ app/javascript/packs/progressBar.js
@@ -1,35 +1,38 @@
-import { Circle } from 'progressbar.js';
-
-var goalsList = document.querySelectorAll("#avatar-progress")
-goalsList.forEach(function(goal){
+import { Circle } from "progressbar.js";
+var goalsList = document.querySelectorAll("#avatar-progress");
+goalsList.forEach(function (goal) {
   var circle = new Circle(goal, {
-    color: '#068140',
-    trailColor: '#eee',
+    color: "#068140",
+    trailColor: "#eee",
     trailWidth: 2,
     duration: 2000,
-    easing: 'bounce',
+    easing: "bounce",
     strokeWidth: 6,
-    from: {color: '#FF0000', a:0},
-    to: {color: '#068140', a:1},
+    from: { color: "#FF0000", a: 0 },
+    to: { color: "#068140", a: 1 },
     warnings: false,
     text: {
-      value: '<div class="avatar-progress-text">'+ Math.round(goal.dataset.percent * 100) +'%</div>',
+      value:
+        '<div class="avatar-progress-text">' +
+        Math.round(goal.dataset.percent * 100) +
+        "%</div>",
       className: "progressbar__label " + goal.dataset.icon,
       style: {
-        color: '#975B53',
-        position: 'absolute',
-        left: '50%',
-        top: '50%',
+        color: "#975B53",
+        position: "absolute",
+        left: "50%",
+        top: "50%",
         padding: 0,
         margin: 0,
         transform: {
-            prefix: true,
-            value: 'translate(-50%, -50%)',
-        }
+          prefix: true,
+          value: "translate(-50%, -50%)",
+        },
+      },
+      step: function (state, circle) {
+        circle.path.setAttribute("stroke", state.color);
+      },
     },
-    step: function(state, circle) {
-      circle.path.setAttribute('stroke', state.color);
-    }
-  }});
+  });
   circle.animate(goal.dataset.percent);
 });
