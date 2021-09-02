62bf8721d48c72cc1eb813aad96d2e52ec1a07bc
oceanelf <70059795+oceanelf@users.noreply.github.com>
2021-09-02 19:26:19 +0100

    spacing

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,7 +27,7 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
-import {goalsTotal} from './numberCounter';
+import { goalsTotal } from './numberCounter';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
