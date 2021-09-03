5ba01aa72a91a8a28e6993a2fd22bf028005e276
Jake Monger <jakemonger18@gmail.com>
2021-09-03 02:00:19 +0100

    Added global listof currencys for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  helper_method :currency
 
   include Pundit
 
@@ -13,6 +14,10 @@ class ApplicationController < ActionController::Base
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to(root_path)
+  end
+
+  def currency
+    ["USD", "GBP", "EUR"]
   end
 
   private
