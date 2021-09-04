01db1b243aada21952a1ae5d73931d0baf25914c
Jake Monger <jakemonger18@gmail.com>
2021-09-04 15:52:31 +0100

    Global Currency Pretty

--- db/schema.rb
+++ db/schema.rb
@@ -73,7 +73,7 @@ ActiveRecord::Schema.define(version: 2021_09_02_232101) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
-    t.string "base_currency", default: "GBP"
+    t.string "base_currency"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
