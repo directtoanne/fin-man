39d4bf1e3e46bcd414e106a2e8030804e56985df
Jake Monger <jakemonger18@gmail.com>
2021-09-09 21:24:20 +0100

    Update counter so can do multiple

--- app/javascript/packs/numberCounter.js
+++ app/javascript/packs/numberCounter.js
@@ -1,19 +1,26 @@
 import { CountUp } from 'countup.js';
 export const goalsTotal = () => {
-  const id = 'jsCounter';
-  const element = document.getElementById(id)
-  if (element) {
-    const number = element.dataset.number
-    const prefix = element.dataset.prefix
-    const countUp = new CountUp(element, number, {
-      decimalPlaces: 2,
-      prefix: prefix,
-    });
+  const counterMain = document.querySelector('.counter-main')
+  console.log(counterMain);
 
-    if (!countUp.error) {
-      countUp.start()
-    } else {
-      console.error(countUp.error);
-    }
-  }
-};
+  if (counterMain) {
+    const elements = document.querySelectorAll('.jsCounter')
+
+    if (elements) {
+      elements.forEach(element => {
+        const number = element.dataset.number
+        const prefix = element.dataset.prefix
+        const countUp = new CountUp(element, number, {
+          decimalPlaces: 2,
+          prefix: prefix,
+        });
+
+        if (!countUp.error) {
+          countUp.start()
+        } else {
+          console.error(countUp.error);
+        }
+      });
+    };
+  };
+}
