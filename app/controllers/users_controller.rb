4b63704ab65d8862469b7d67307de359a2155a9e
Jake Monger <jakemonger18@gmail.com>
2021-09-09 20:47:46 +0100

    Changes to account stuff and added goals

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,12 @@ class UsersController < ApplicationController
     # authorize @accounts - one is enough, since it authorises the (current) user (in this case, which is not the way to do that)
     authorize @goals
     @accounts_balance = accounts_total
+    @goals_total = goals_total
+    #@goals_data = goals_data
+  end
+
+  def goals_total
+    calculate_goals_total
   end
 
   def accounts_total
@@ -16,22 +22,32 @@ class UsersController < ApplicationController
 
   private
 
+  def calculate_goals_total
+    goals_total = 0
+    if @goals != nil? || @goals.count.positive?
+      @goals.each do |goal|
+        goals_total += goal.current_amount
+      end
+    end
+    return goals_total
+  end
+
   def convert_account_balances
-    total = 0
-    if @accounts
+    accounts_total = 0
+    if @accounts != nil? || @accounts.count.positive?
       @accounts.each do |account|
         if current_user.base_currency == account.currency
-          total += account.balance
+          accounts_total += account.balance
         else
           api_key = ENV["EXCHANGE_RATE_API_KEY"]
           url = "https://v6.exchangerate-api.com/v6/#{api_key}/pair/#{current_user.base_currency}/#{account.currency}/#{account.balance}"
           uri = URI(url)
           response = Net::HTTP.get(uri)
           response_obj = JSON.parse(response)
-          total += response_obj['conversion_result'].round(2)
+          accounts_total += response_obj['conversion_result'].round(2)
         end
       end
-      return total
+      return accounts_total
     end
   end
 end
